563957d1ba48a12bcb30b0db692b4ad815af1f26
linn <linncoffeee@gmail.com>
2023-05-05 17:18:07 +0630

    align store/update button to the right for better ux

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -5,68 +5,48 @@
 @endsection
 
 @section('content')
-
     <h4>
         Create New Book
     </h4>
 
-    <form action="{{ route("book.store") }}" method="post">
+    <form action="{{ route('book.store') }}" method="post">
         @csrf
 
         <div class="mb-3">
             <label for="" class=" form-label">
                 Name
             </label>
-            <input
-            type="text"
-            name="name"
-            value=""
-            class=" form-control">
+            <input type="text" name="name" value="" class=" form-control">
         </div>
         <div class="mb-3">
             <label for="" class=" form-label">
                 Price
             </label>
-            <input
-            type="number"
-            name="price"
-            value="0"
-            step="50"
-            class=" form-control">
+            <input type="number" name="price" value="0" step="50" class=" form-control">
         </div>
         <div class="mb-3">
             <label for="" class=" form-label">
                 Course
             </label>
             {{-- yellow, turn me into datalist --}}
-            <input
-            type="text"
-            name="course"
-            value=""
-            class=" form-control">
+            <input type="text" name="course" value="" class=" form-control">
         </div>
         <div class="mb-3">
             <label for="" class=" form-label">
                 Stock
             </label>
-            <input
-            type="number"
-            name="stock"
-            value="0"
-            class=" form-control">
+            <input type="number" name="stock" value="0" class=" form-control">
         </div>
         <div class="mb-3">
             <label for="" class=" form-label">
                 Minimum Stock
             </label>
-            <input
-            type="number"
-            name="minStock"
-            value="0"
-            class=" form-control">
+            <input type="number" name="minStock" value="0" class=" form-control">
         </div>
-        <button class=" btn btn-primary">
-            Store
-        </button>
+        <div class=" d-flex justify-content-end">
+            <button class=" btn btn-primary">
+                Store
+            </button>
+        </div>
     </form>
 @endsection
--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -5,69 +5,49 @@
 @endsection
 
 @section('content')
-
     <h4>
         Edit New Book
     </h4>
 
-    <form action="{{ route("book.update", $book->id) }}" method="post">
+    <form action="{{ route('book.update', $book->id) }}" method="post">
         @csrf
-        @method("put")
+        @method('put')
 
         <div class="mb-3">
             <label for="" class=" form-label">
                 Name
             </label>
-            <input
-            type="text"
-            name="name"
-            value="{{ $book->name }}"
-            class=" form-control">
+            <input type="text" name="name" value="{{ $book->name }}" class=" form-control">
         </div>
         <div class="mb-3">
             <label for="" class=" form-label">
                 Price
             </label>
-            <input
-            type="number"
-            name="price"
-            value="{{ $book->price }}"
-            step="50"
-            class=" form-control">
+            <input type="number" name="price" value="{{ $book->price }}" step="50" class=" form-control">
         </div>
         <div class="mb-3">
             <label for="" class=" form-label">
                 Course
             </label>
             {{-- yellow, turn me into datalist --}}
-            <input
-            type="text"
-            name="course"
-            value="{{ $book->course }}"
-            class=" form-control">
+            <input type="text" name="course" value="{{ $book->course }}" class=" form-control">
         </div>
         <div class="mb-3">
             <label for="" class=" form-label">
                 Stock
             </label>
-            <input
-            type="number"
-            name="stock"
-            value="{{ $book->stock }}"
-            class=" form-control">
+            <input type="number" name="stock" value="{{ $book->stock }}" class=" form-control">
         </div>
         <div class="mb-3">
             <label for="" class=" form-label">
                 Minimum Stock
             </label>
-            <input
-            type="number"
-            name="minStock"
-            value="{{ $book->minStock }}"
-            class=" form-control">
+            <input type="number" name="minStock" value="{{ $book->minStock }}" class=" form-control">
         </div>
-        <button class=" btn btn-primary">
-            Update
-        </button>
+        <div class=" d-flex justify-content-end">
+            <button class=" btn btn-primary float-right">
+                Update
+            </button>
+        </div>
     </form>
 @endsection
