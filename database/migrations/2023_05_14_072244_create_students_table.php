d3022ba5518e715b223a49f0cc2f5707d2476d01
linn <linncoffeee@gmail.com>
2023-05-14 13:56:35 +0630

    add migration for Student model

---
+++ database/migrations/2023_05_14_072244_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 20);
+            $table->string('roll_no', 8);
+            // yellow, i should be using enum but want to speed things up so doing that later
+            $table->string('paid', 3);
+            $table->foreignId("course_id")
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
